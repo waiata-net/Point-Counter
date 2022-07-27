e67a048ad848d52c547cf38be41704ae92801b27
Neal <neal@lekandu.net>
2022-07-27 19:53:52 +0800

    created Scoring

--- Model/Scoring.swift
+++ Model/Scoring.swift
@@ -9,4 +9,22 @@ import Foundation
 
 struct Scoring: Codable {
     
+    var title: String = ""
+    var method: Method = .tally
+    var start: Int = 0
+    var step: Int = 1
+    var value: Int = 1
+    var target: Int?
+    var margin: Int?
+    var strings: [String] = []
+    var successor: [Scoring]
+    
+    
+    enum Method: Codable {
+        case firstTo
+        case bestOf
+        case series
+        case tally
+        case total
+    }
 }
